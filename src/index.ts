7189af5f79448bc8233499f00f32a8d0f79dd6c8
elshafie <m_elshafie15@yahoo.com>
2024-07-30 01:59:19 +0300

    push food ordering app backend

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,23 @@ import express, {Response, Request} from "express"
 import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose"
+import {v2 as cloudinary} from "cloudinary"
 
 import usersRoute from './routes/usersRoute'
+import myRestaurantRoute from "./routes/myRestaurantRoute";
+
 import { jwtCheck } from "./middleware/auth"
 
 
 mongoose
   .connect(process.env.MONGO_URL as string)
   .then(() => (console.log("Database Connected Successfully")));
+
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
 const app = express()
 
@@ -19,6 +28,7 @@ app.use(jwtCheck);
 
 //  /api/user
 app.use("/api/user", usersRoute)
+app.use("/api/restaurant", myRestaurantRoute);
 
 app.listen(7000, () => {
   console.log("Server is running on port 7000")
